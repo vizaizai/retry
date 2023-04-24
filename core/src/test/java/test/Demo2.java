e66e8676a4ac0a338efb700b28c8d1f95c5dbc06
liaochongwei <liaochongwei666@163.com>
2023-04-24 17:52:18 +0800

    移除多余代码

---
+++ core/src/test/java/test/Demo2.java
@@ -0,0 +1,30 @@
+package test;
+
+import com.github.vizaizai.logging.LoggerFactory;
+import com.github.vizaizai.retry.timewheel.HashedWheelTimer;
+import com.github.vizaizai.retry.util.Utils;
+import org.slf4j.Logger;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author liaochongwei
+ * @date 2022/1/5 10:51
+ */
+public class Demo2 {
+    private static final Logger log = LoggerFactory.getLogger(Demo2.class);
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        HashedWheelTimer hashedWheelTimer = new HashedWheelTimer(200,2048, Runtime.getRuntime().availableProcessors());
+
+        hashedWheelTimer.newTimeout(()->{
+            System.out.println("12123");
+        },200, TimeUnit.MILLISECONDS);
+    }
+}
--- vi-remote/src/main/java/com/github/vizaizai/remote/client/netty/NettyConnectionPool.java
+++
@@ -1,8 +0,0 @@
-package com.github.vizaizai.remote.client.netty;
-
-/**
- * @author liaochongwei
- * @date 2022/2/21 11:17
- */
-public class NettyConnectionPool {
-}
